1ca8e4a800c62c6e1bc20f7590f564912799578d
Code_Builder <codebuilder24@gmail.com>
2024-07-04 02:15:58 +0000

    Starting The Project

---
+++ Script.js
@@ -0,0 +1,3 @@
+/*The tribute page should contain the print button and a button that calculates the age of the person.
+To add more javascript to the code , add a button that gives the persons age, name , date of birth and location after it has been clicked on
+*/

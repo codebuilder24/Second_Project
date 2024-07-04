78e41bde28b15066e4ac268e43574b75b020ca15
Code_Builder <codebuilder24@gmail.com>
2024-07-04 18:25:11 +0000

    Cooking The Tribute Page

--- Script.js
+++ Script.js
@@ -1,3 +1,10 @@
 /*The tribute page should contain the print button and a button that calculates the age of the person.
 To add more javascript to the code , add a button that gives the persons age, name , date of birth and location after it has been clicked on
 */
+function myName (){
+    let name =  "Michael Joseph Jackson";
+    let nickName = "King Of Pop";
+    let DOB = "August 29, 1958";
+    let age  = 2009-1958;
+    document.write(name, age, year);
+}

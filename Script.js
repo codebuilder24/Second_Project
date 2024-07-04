edb5df820ee619bae2c10f04972607d8fa9952a0
Code_Builder <codebuilder24@gmail.com>
2024-07-04 23:57:30 +0000

    Done With The Second Project

--- Script.js
+++ Script.js
@@ -1,14 +1,11 @@
-/*The tribute page should contain the print button and a button that calculates the age of the person.
-To add more javascript to the code , add a button that gives the persons age, name , date of birth and location after it has been clicked on
-*/
-
 function myName (){
     let name =  "Michael Joseph Jackson";
     let nickName = "King Of Pop";
     let DOB = "August 29, 1958";
     let age  = 2009-1958;
-    document.write(name, age, year);
+    window.alert("Name is "+name)
+    window.alert("Nickname is "+nickName)
+    window.alert("Date Of Birth Is "+DOB)
+    window.alert("His age is "+ age)
+    window.print()
 }
-//Using the winwdow.promt to issue a commannd that will ask the user what he/ she wants to know about the KOP
-//You have learnt how to use the alert , document write , console log and window.print
-//Asking for the names
